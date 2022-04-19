ff6c8126e935974e3a0a8295a18b38f682b99ca8
SaD-Pr0ger <sad.pr0ger@gmail.com>
2022-04-19 19:00:38 +0300

    updated config, models, readme and added alembic

--- data/config.py
+++ data/config.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOTS_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
 DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+BASE = declarative_base()
--- utils/db/models.py
+++ utils/db/models.py
@@ -1,18 +1,13 @@
-from sqlalchemy import create_engine, Column, Integer, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Boolean
 
-from data.config import DATABASE_URL
-
-
-engine = create_engine(DATABASE_URL)
-BASE = declarative_base()
+from data.config import BASE, engine
 
 
 class Subscribers(BASE):
     __tablename__ = "subscribers"
 
     ID = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
+    user_id = Column(Integer, nullable=False)
     is_subscribed = Column(Boolean, default=False)
 
 
