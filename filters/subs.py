83958f5200871de06a4f6c835ce3a23fb0758cad
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-05-18 17:24:45 +0500

    updated filters

--- filters/subs.py
+++ filters/subs.py
@@ -6,9 +6,11 @@ from loader import DB
 
 class IsSubscribed(BoundFilter):
     async def check(self, message: Message) -> bool:
-        return DB.retrieve_user(message.from_user.id).is_subscribed is True
+        user = DB.retrieve_user(message.from_user.id)
+        return user.is_subscribed is True if user else None
 
 
 class IsNotSubscribed(BoundFilter):
     async def check(self, message: Message) -> bool:
-        return DB.retrieve_user(message.from_user.id).is_subscribed is False
+        user = DB.retrieve_user(message.from_user.id)
+        return user.is_subscribed is False if user else None
